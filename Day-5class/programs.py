5ba8b4607bff3dce57c7ec37e7c801cd5291aacb
indunadella <indu.nadella1122@gmail.com>
2025-05-28 16:14:07 +0530

    Add files via upload

---
+++ Day-5class/programs.py
@@ -0,0 +1,26 @@
+P=900
+T=8
+R=20
+SI=(P*T*R)/100      # simple interest
+print("Simple Interest=",SI)
+
+C=32
+F=(C*(9/5)+32)      # conversion of celsius to fahrenheit
+print("Fahrenheit=",F)
+
+a=34
+b=46
+c=79
+avg=(a+b+c)/3       # average of three numbers
+print("average of 3 numbers=",avg)
+
+r=7.8
+area=3.14*r**2      # area of circle
+print("area of circle=",area)
+
+a=6
+b=9
+temp=a              # swapping of two variables
+a=b
+b=temp
+print("swapping of two variables=",a,b)
